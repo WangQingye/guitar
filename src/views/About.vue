13ea60e05ec1aea3726a6c610b23a5afbe8404f5
wangqingye <wangqingye@myoas.com>
2020-04-28 14:36:26 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Swipe :class="['my-swipe']" autoplay="0" indicator-color="white">
+      <Swipe-Item class="my-swipe-item" v-for="(img, i) in state.urls" :key="i">
+        <img class="swipe-img" :src="img">
+      </Swipe-Item>
+    </Swipe>
   </div>
 </template>
+
+<script lang="ts">
+import { reactive, onMounted } from '@vue/composition-api'
+import { Swipe, SwipeItem } from 'vant'
+import guitars from '@/guitar.json'
+export default {
+  components: {
+    Swipe,
+    SwipeItem,
+  },
+  setup(props, context) {
+    const state = reactive({
+      title: '',
+      urls: [],
+    })
+    onMounted(() => {
+      state.title = context.root.$route.params.keyName
+      state.urls = guitars[state.title].urls
+    })
+    return { state }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.about {
+  width: 100vw;
+  height: 95vh;
+}
+.my-swipe {
+  min-height: 100px;
+  background: rgb(102, 198, 242);
+  .my-swipe-item {
+    .swipe-img {
+      max-width: 100%;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p class="title">
+      <Icon name="music" class="icon" />
+      <span class="title-text">{{state.testNum}}</span>
+      <span class="title-text">清野吉他铺</span>
+    </p>
+    <ul class="list-ul">
+      <li class="list-li" v-for="(item,i) in state.list" :key="i" @click="goImg(item)">
+        <p class="list-p">{{item}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { Icon } from 'vant'
+import { reactive, onMounted } from '@vue/composition-api'
+import router from '@/router'
+import guitars from '@/guitar.json'
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
-  }
-};
+    Icon,
+  },
+  setup() {
+    const state = reactive({
+      list: [],
+    })
+    function goImg(item) {
+      router.push({ path: `/about/${item}`})
+    }
+    onMounted(()=> {
+      for (const key in guitars) {
+        state.list.push(key)
+      }
+    })
+    return { state, goImg }
+  },
+}
 </script>
+<style lang="scss" scoped>
+.title {
+  line-height: 40px !important;
+  height: 40px;
+  font-size: 30px;
+  margin-top: 20px;
+  .icon {
+    vertical-align: middle;
+    margin-right: 8px;
+  }
+  .title-text {
+    font-size: 32px;
+    vertical-align: middle;
+  }
+}
+.list-ul {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  padding: 0 10px;
+  .list-li {
+    margin-top: 20px;
+    width: 120px;
+    background: white;
+    border-radius: 10px;
+    height: 20px;
+    padding: 5px;
+    box-shadow: 3px 3px 3px rgb(219, 219, 219);
+    .list-p {
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+  }
+}
+</style>
